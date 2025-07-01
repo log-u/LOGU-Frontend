75374f563f35a69ac61350a1c57e637782cf3b52
Chaejy <xhdrp@sookmyung.ac.kr>
2025-07-01 18:06:17 +0900

    chore: 프로젝트 초기 설정

---
+++ next.config.ts
@@ -0,0 +1,24 @@
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.ts',
+        },
+      },
+    },
+  },
+};
+
+export default nextConfig;
